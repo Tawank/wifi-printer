dc5cb6f3e249847696b7b28fed6c165b90ecb77c
Karol Stawowski <kstawowski@gmail.com>
2022-09-05 12:37:08 +0200

    Fix style in MyWebServer

--- src/MyWebServer.cpp
+++ src/MyWebServer.cpp
@@ -8,15 +8,37 @@ extern Adafruit_Thermal printer;
 extern WebServer server;
 
 const char PROGMEM *index_html_data =
-    "<!DOCTYPE html><html>"
-    "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
-    "<link rel=\"icon\" href=\"data:,\">"
-    "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}"
-    ".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;"
-    "text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}"
-    ".button2 {background-color: #555555;}</style></head>"
-    "<body><h1>OK</h1>"
-    "</body></html>";
+    "<!DOCTYPE html>"
+    "<html>"
+      "<head>"
+        "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
+        "<link rel=\"icon\" href=\"data:,\">"
+        "<style>"
+          "html {"
+            "font-family: Helvetica;"
+            "display: inline-block;"
+            "margin: 0px auto;"
+            "text-align: center;"
+          "}"
+          ".button {"
+            "background-color: #4CAF50;"
+            "border: none;"
+            "color: white;"
+            "padding: 16px 40px;"
+            "text-decoration: none;"
+            "font-size: 30px;"
+            "margin: 2px;"
+            "cursor: pointer;"
+          "}"
+          ".button2 {"
+            "background-color: #555555;"
+          "}"
+        "</style>"
+      "</head>"
+      "<body>"
+        "<h1>OK</h1>"
+      "</body>"
+    "</html>";
 
 void urlHandleIndex() {
   server.send(200, "text/html", index_html_data);
